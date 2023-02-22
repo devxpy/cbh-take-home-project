ff2039608e48a79cbb28a9d8bfeb689d5dac08fd
Dev Aggarwal <devxpy@gmail.com>
2023-02-23 04:48:33 +0530

    refactor: reduce number of variables

--- dpk.js
+++ dpk.js
@@ -14,25 +14,20 @@ exports.deterministicPartitionKey = (event) => {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  const givenPartitionKey = event.partitionKey;
+  let givenPartitionKey = event.partitionKey;
 
   // an explicit partition key is not provided, simply return the hash
   if (!givenPartitionKey) {
     return sha3_512(JSON.stringify(event));
   }
 
-  let candidate = givenPartitionKey;
-  // short-circuit empty inputs
-  if (!candidate) {
-    return TRIVIAL_PARTITION_KEY;
-  }
   // avoid re-encoding inputs
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
+  if (typeof givenPartitionKey !== "string") {
+    givenPartitionKey = JSON.stringify(givenPartitionKey);
   }
   // given partitionKey is too long, force hash it!
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = sha3_512(candidate);
+  if (givenPartitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    givenPartitionKey = sha3_512(givenPartitionKey);
   }
-  return candidate;
+  return givenPartitionKey;
 };
